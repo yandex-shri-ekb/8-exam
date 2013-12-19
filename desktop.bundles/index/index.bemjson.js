198bead120d1da0cbb80a49121759a621b475b7a
Alexander Serditov <av.serditov@gmail.com>
2013-12-19 10:53:57 +0600

    change anygrid.css to use bem-style of grid in index.bemjson

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -26,7 +26,7 @@
                                         },
                                         {
                                             elem: 'hero',
-                                            mix: { block: 'g-col-2 g-span-10' },
+                                            mix: { block: 'grid__module', mods: {span: 10, col: 2} },
                                             content: '- А что если интернет подстроится под меня?'
                                         },
                                     ]
@@ -36,12 +36,12 @@
                                     content: [
                                         {
                                             block: 'lead-text',
-                                            mix: { block: 'g-col-2 g-span-6' },
+                                            mix: { block: 'grid__module', mods: {span: 6, col: 2} },
                                             content: 'Интернет сегодня — это не только информация и технологии, но и люди. Людей много и они все разные, а сайты для всех пользователей обычно выглядят одинаково. Яндекс уже научился адаптироваться под конкретного пользователя и готов поделиться этой технологией с другими. <br/><br/> «Атом». Технологическая платформа, задача которой — предоставить сайтам возможность изменять содержание страниц под конкретного пользователя.'
                                         },
                                         {
                                             elem: 'video-holder',
-                                            mix: { block: 'g-col-9 g-span-3' },
+                                            mix: { block: 'grid__module', mods: {span: 3, col: 9} },
                                             content: [
                                                 {
                                                     elem: 'video-image'
@@ -59,19 +59,22 @@
                                     content: [
                                         {
                                             block: 'user',
-                                            mix: { block: 'g-col-2 g-span-4' },
+                                            mix: { block: 'grid__module', mods: {span: 4, col: 2} },
+                                            type: 'yellow',
                                             name: 'Максим',
                                             text: 'Предпочитает путешествовать самостоятельно и налегке. Уверен, что только так можно проникнуться культурой страны.'
                                         },
                                         {
                                             block: 'user',
-                                            mix: { block: 'g-col-5 g-span-4' },
+                                            mix: { block: 'grid__module', mods: {span: 4, col: 5} },
+                                            type: 'red',
                                             name: 'Алексей',
                                             text: 'В отпуск ездит вместе с женой и дочкой. Считает, что отдыхать лучше всего в отелях на побережье..'
                                         },
                                         {
                                             block: 'user',
-                                            mix: { block: 'g-col-8 g-span-4' },
+                                            mix: { block: 'grid__module', mods: {span: 4, col: 8} },
+                                            type: 'blue',
                                             name: 'Григорий И. Дауге',
                                             text: 'Очень дорожит редкими отпусками и превыше всего ценит комфорт и качество обслуживания..'
                                         },
