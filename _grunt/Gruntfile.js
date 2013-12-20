01a999ba9f623c83420e1e6146bf7a9fc6b70b28
mrsamo <mr--alex@yandex.ru>
2013-12-20 06:18:59 +0600

    Инициализация репозитория 1. Сетка 2. Верстка шапки

---
+++ _grunt/Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        watch: {
+            compass: {
+                files: ['../css/sass/*.scss'],
+                tasks: ['compass']
+            }
+        },
+        compass: {
+            main: {
+                options: {
+                    sassDir: '../css/sass',
+                    cssDir: '../css',
+                    imagesDir: '../img',
+                    javascriptsDir: '../js',
+                    outputStyle: 'expanded',
+                    debugInfo: true
+                }
+            }
+        },
+        csscomb: {
+            main: {
+                options: {
+                    sortOrder: './.csscomb.json'
+                },
+                files: {
+                    '../css/main.css': ['../css/main.css']
+                }
+            }
+        }
+    });
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.registerTask('default', ['compass', 'csscomb', 'watch']);
+};
