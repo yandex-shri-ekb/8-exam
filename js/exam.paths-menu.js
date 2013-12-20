08e98974d8ed71f1d5986c51db0923007aeaa9a3
Vladimir Shestakov <boolive@yandex.ru>
2013-12-21 01:05:38 +0600

    Автоскролл из нижнего меню

--- js/exam.paths-menu.js
+++ js/exam.paths-menu.js
@@ -4,16 +4,27 @@
  * @date 20.12.2013
  * @author Vladimir Shestakov <boolive@yandex.ru>
  */
-(function($, undefined) {
+(function($) {
     $.widget("exam.paths-menu", $.boolive.widget, {
         _create: function() {
             $.boolive.widget.prototype._create.call(this);
             var self = this;
+            // В опциях не указан селектор элемента, до которого скролить
+            if (typeof this.options.scrollto === 'undefined'){
+                this.options.scrollto = false;
+            }
             this.element.on('click', '.paths-menu__item', function(e){
                 if (!$(this).hasClass('paths-menu__item_state_active')){
                     e.preventDefault();
-                    // скрытие боковой панели
+                    // сообщаем всем о выборе
                     self.callParents('select_path', $(this).attr('data-type'), null, true);
+                    // автосколл до указанного элемента
+                    if (self.options.scrollto){
+                        var pos_elem = $(self.options.scrollto+':first');
+                        if (pos_elem.size()){
+                            $('html,body').animate({ scrollTop: pos_elem.offset().top }, 'slow');
+                        }
+                    }
                 }
             })
             .on('mouseenter', '.paths-menu__item', function(e){
