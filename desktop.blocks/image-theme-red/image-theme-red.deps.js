34ca2b1f5e957ecdfe461e10c30103cb4fecdf2b
Alexander Serditov <av.serditov@gmail.com>
2013-12-21 00:16:45 +0600

    add red theme

---
+++ desktop.blocks/image-theme-red/image-theme-red.deps.js
@@ -0,0 +1,25 @@
+({
+    shouldDeps: [
+        { elem: 'bag' },
+        { elem: 'book' },
+        { elem: 'cards' },
+        { elem: 'cup' },
+        { elem: 'ipad' },
+        { elem: 'iphone' },
+        { elem: 'krug' },
+        { elem: 'maska' },
+        { elem: 'money' },
+        { elem: 'newspaper' },
+        { elem: 'notebook2' },
+        { elem: 'notepad' },
+        { elem: 'palka' },
+        { elem: 'paper' },
+        { elem: 'pen' },
+        { elem: 'phone' },
+        { elem: 'photo' },
+        { elem: 'pie' },
+        { elem: 'ticket' },
+        { elem: 'watch' },
+        { elem: 'yoga' }
+    ]
+})
---
+++ desktop.blocks/story-theme-red/story-theme-red.deps.js
@@ -0,0 +1,7 @@
+({
+    shouldDeps: [
+        { block: 'image-theme-red' },
+        { block: 'path-theme-red' },
+        { block: 'step' },
+    ]
+})
