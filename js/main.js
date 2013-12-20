033f043c0162f5e149e86f0f1a9adfe6941b4f6d
mrsamo <mr--alex@yandex.ru>
2013-12-20 08:46:19 +0600

    Начало расстановки блоков на желтом персонаже

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
-$(function () {
+var person = {
+    hiddenClass: 'i-invisible',
+    init: function () {
+        var _this = this;
 
+        _this.initToggleShowShortPersonDesc();
+    },
+    initToggleShowShortPersonDesc: function () {
+        var _this = this;
+
+        $('.js-person-icon')
+            .mouseover(function () {
+                var $this = $(this),
+                    $personDescription = $($this.data('person-description')),
+                    $personsDescription = $('.js-person-description');
+
+                $personsDescription.addClass(_this.hiddenClass);
+                $personDescription.removeClass(_this.hiddenClass);
+            })
+            .mouseout(function () {
+                var $this = $(this),
+                    $personDescription = $($this.data('person-description'));
+
+                $personDescription.addClass(_this.hiddenClass);
+            });
+    }
+};
+
+$(function () {
+    person.init();
 });
