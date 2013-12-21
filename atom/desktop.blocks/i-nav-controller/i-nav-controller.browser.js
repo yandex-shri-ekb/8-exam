94b008157c069512899e809ee894aac475de1e3c
Anton Zhevak <antzhe@gmail.com>
2013-12-22 03:21:42 +0600

    i-nav-controller, i-scroll-controller: обработчики onscroll вынесены в метод

--- atom/desktop.blocks/i-nav-controller/i-nav-controller.browser.js
+++ atom/desktop.blocks/i-nav-controller/i-nav-controller.browser.js
@@ -4,23 +4,29 @@ modules.define('i-bem__dom', ['jquery'], function(provide, $, BEMDOM) {
         onSetMod: {
             js: {
                 inited: function() {
-                    var steps = this.findBlocksInside('steps'),
-                        nav = this.findBlockInside('nav'),
-                        self = this;
+                    this._steps = this.findBlocksInside('steps');
+                    this._nav = this.findBlockInside('nav');
+                    this._page = this.findBlockOn('page');
 
-                    $(window).on('scroll', function() {
-                        // TODO: брать только текущий steps
-                        for(var i = 0, len_i = steps.length; i < len_i; i++) {
-                            var items = steps[i].elem('item');
-                            for(var j = 0, len_j = items.length; j < len_j; j++) {
-                                var rect = items[j].getBoundingClientRect();
-                                if (rect.top < 100 && rect.bottom > 100) {
-                                    var num = steps[i].getMod($(items[j]), 'num');
-                                    nav.setMod('num', num);
-                                }
-                            }
-                        }
-                    });
+                    $(window).on('scroll', $.proxy(this._onWindowScroll, this));
+                }
+            }
+        },
+        _onWindowScroll: function() {
+            var currentColor = this._page.getMod('color'),
+                steps = this._steps;
+
+            for(var i = 0, len_i = steps.length; i < len_i; i++) {
+                if(steps[i].getMod('color') !== currentColor)
+                    continue;
+
+                var items = steps[i].elem('item');
+                for(var j = 0, len_j = items.length; j < len_j; j++) {
+                    var rect = items[j].getBoundingClientRect();
+                    if (rect.top < 100 && rect.bottom > 100) {
+                        var num = steps[i].getMod($(items[j]), 'num');
+                        this._nav.setMod('num', num);
+                    }
                 }
             }
         }
--- atom/desktop.blocks/i-scroll-controller/i-scroll-controller.browser.js
+++ atom/desktop.blocks/i-scroll-controller/i-scroll-controller.browser.js
@@ -4,25 +4,26 @@ modules.define('i-bem__dom', ['jquery'], function(provide, $, BEMDOM) {
         onSetMod: {
             js: {
                 inited: function() {
-                    var windowHeight = $(window).height(),
-                        persons = this.findBlocksInside('persons'),
-                        person = this.findBlockInside('person'),
-                        nav = this.findBlockInside('nav');
+                    this._windowHeight = $(window).height(),
+                    this._persons = this.findBlocksInside('persons'),
+                    this._person = this.findBlockInside('person'),
+                    this._nav = this.findBlockInside('nav');
 
-                    $(window).on('scroll', function(e) {
-                        var scrollTop = $(window).scrollTop(),
-                            from = persons[0].domElem.offset().top + persons[0].domElem.outerHeight(),
-                            to = persons[1].domElem.offset().top;
-
-                        if(scrollTop < from || (scrollTop + windowHeight) > to) {
-                            person.setMod('status', 'hided');
-                            nav.setMod('status', 'hided');
-                        } else if(scrollTop > from && (scrollTop + windowHeight) < to) {
-                            person.setMod('status', 'showed');
-                            nav.setMod('status', 'showed');
-                        }
-                    });
+                    $(window).on('scroll', $.proxy(this._onWindowScroll, this));
                 }
+            }
+        },
+        _onWindowScroll: function() {
+            var scrollTop = $(window).scrollTop(),
+                from = this._persons[0].domElem.offset().top + this._persons[0].domElem.outerHeight(),
+                to = this._persons[1].domElem.offset().top;
+
+            if(scrollTop < from || (scrollTop + this._windowHeight) > to) {
+                this._person.setMod('status', 'hided');
+                this._nav.setMod('status', 'hided');
+            } else if(scrollTop > from && (scrollTop + this._windowHeight) < to) {
+                this._person.setMod('status', 'showed');
+                this._nav.setMod('status', 'showed');
             }
         }
     });
