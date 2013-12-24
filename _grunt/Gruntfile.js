4cef8cfc272f031c46da6c2ae6ccc74101b6d6d0
mrsamo <mr--alex@yandex.ru>
2013-12-24 13:38:58 +0600

    Добавлена оптимизация CSS, JS (минимизация, сжатие), оптимизация картинок, разделение на девелоперскую, продакш и проверочную версию )

--- _grunt/Gruntfile.js
+++ _grunt/Gruntfile.js
@@ -4,34 +4,192 @@ module.exports = function (grunt) {
         watch: {
             compass: {
                 files: ['../css/sass/*.scss'],
-                tasks: ['compass']
+                tasks: ['compass:development']
             }
         },
         compass: {
-            main: {
+            development: {
                 options: {
                     sassDir: '../css/sass',
-                    cssDir: '../css',
+                    cssDir: '../css/development',
                     imagesDir: '../img',
+                    generatedImagesDir: '../img/spritegen',
                     javascriptsDir: '../js',
                     outputStyle: 'expanded',
-                    debugInfo: true
+                    debugInfo: true,
+                    relativeAssets: true
+                }
+            },
+            control: {
+                options: {
+                    sassDir: '../css/sass',
+                    cssDir: '../css/control',
+                    imagesDir: '../img',
+                    generatedImagesDir: '../img/spritegen',
+                    javascriptsDir: '../js',
+                    outputStyle: 'expanded',
+                    debugInfo: false,
+                    relativeAssets: true
+                }
+            },
+            production: {
+                options: {
+                    sassDir: '../css/sass',
+                    cssDir: '../css/production',
+                    imagesDir: '../img',
+                    generatedImagesDir: '../img/spritegen',
+                    javascriptsDir: '../js',
+                    outputStyle: 'compressed',
+                    debugInfo: false,
+                    relativeAssets: true
                 }
             }
         },
+        concat: {
+            production: {
+                src: [
+                    '../css/vendor/normalize.css',
+                    '../css/production/fonts.css',
+                    '../css/production/base.css',
+                    '../css/production/main.css',
+                    '../css/production/yellow.css',
+                    '../css/production/red.css',
+                    '../css/production/blue.css'
+                ],
+                dest: '../css/production/all.css'
+            },
+            control: {
+                src: [
+                    '../css/vendor/normalize.css',
+                    '../css/production/fonts.css',
+                    '../css/control/base.css',
+                    '../css/control/main.css',
+                    '../css/control/yellow.css',
+                    '../css/control/red.css',
+                    '../css/control/blue.css'
+                ],
+                dest: '../css/control/all.css'
+            }
+        },
         csscomb: {
-            main: {
-                options: {
-                    sortOrder: '.csscomb.json'
-                },
+            options: {
+                sortOrder: '.csscomb.json'
+            },
+            production: {
                 files: {
-                    '../css/main.css': ['../css/main.css'],
-//                    '../css/sass/main.scss': ['../css/sass/main.scss']
+                    '../css/production/all.css': ['../css/production/all.css']
                 }
+            },
+            control: {
+                files: {
+                    '../css/control/all.css': ['../css/control/all.css']
+                }
+            }
+        },
+        cssmin: {
+            production: {
+                expand: true,
+                keepSpecialComments: 0,
+                cwd: '../css/production/',
+                src: ['all.css'],
+                dest: '../css/production/',
+                ext: '.min.css'
+            }
+        },
+        uglify: {
+            production: {
+                files: {
+                    '../js/main.min.js': ['../js/main.js']
+                }
+            }
+        },
+        htmlclean: {
+            production: {
+                expand: true,
+                cwd: '../',
+                src: 'main.html',
+                dest: '../html_clear'
+            }
+        },
+        compress: {
+            production: {
+                options: {
+                    mode: 'gzip'
+                },
+                files: [
+                    {
+                        expand: true,
+                        src: ['../js/main.min.js'],
+                        dest: '../js/',
+                        ext: '.gz.js'
+                    },
+                    {
+                        expand: true,
+                        src: ['../css/production/all.min.css'],
+                        dest: '../css/',
+                        ext: '.gz.css'
+                    }
+                ]
+            }
+        },
+        imagemin: {
+            files: [
+                {
+                    expand: true,
+                    cwd: '../img/',
+                    src: ['**/*.{png,jpg}'],
+                    dest: '../img/'
+                }
+            ]
+        },
+        pngmin: {
+            compile: {
+                options: {
+                    concurrency: 8,
+                    ext: '.png',
+                    quality: '65-80',
+                    speed: 10
+                },
+                files: [
+                    {
+                        expand: true,
+                        src: ['**/*.png'],
+                        cwd: '../img/',
+                        dest: '../img/'
+                    }
+                ]
             }
         }
     });
 
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-    grunt.registerTask('default', ['compass', 'csscomb', 'watch']);
+
+    grunt.registerTask('default', [
+        'compass:development',
+        'watch'
+    ]);
+
+    grunt.registerTask('control', [
+        'compass:control',
+        'concat:control',
+        'csscomb:control'
+    ]);
+
+    grunt.registerTask('production', [
+        'compass:production',
+        'concat:production',
+        'csscomb:production',
+        'cssmin:production',
+        'htmlclean',
+        'uglify',
+        'compress'
+    ]);
+
+    grunt.registerTask('imagemin', [
+        'imagemin:production'
+    ]);
+
+    grunt.registerTask('pngmin', [
+        'pngmin'
+    ]);
 };
